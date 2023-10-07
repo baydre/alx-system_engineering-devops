8ee1ca96b139a95a79da4d066b66442359ae9430
baydre_africa <baydreafrica@gmail.com>
2023-10-07 11:19:14 +0100

    ruby script that should output certain information

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,7 @@
 #!/usr/bin/env ruby
-puts ARGV[].scan(/\[(?:from|to|flags):(.*?)\]/).join(',')
+
+if ARGV.length == 1
+  sender, number, flags = ARGV[0].match(/^.*\[from:(.*?)\].*\[to:(.*?)\].*\[flags:(.*?)\].*$/i).captures
+  puts "#{sender},#{number},#{flags}"
+  exit
+end
