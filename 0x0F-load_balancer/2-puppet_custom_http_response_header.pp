d5f674b25ec4716598c6d06e09368b72b5edd7f2
baydre_africa <baydreafrica@gmail.com>
2023-12-11 07:01:12 +0100

    puppet script that adds a custom HTTP header

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,27 @@
+# script that adds custom http header using puppet.
+
+exec {'update_system':
+  command => '/usr/bin/apt-get -y update',
+}
+
+package { 'nginx':
+  ensure => 'installed',
+}
+
+service { 'nginx':
+  ensure  => 'running',
+  enable  => true,
+  require => Package['nginx'],
+}
+
+file { '/var/www/html/index.html' :
+  content => 'Hello World!',
+}
+
+file_line { 'Creating header':
+  	ensure => present,
+  	path   => '/etc/nginx/sites-available/default',
+  	line   => "\tadd_header X-Served-By ${hostname};",
+  	after  => 'server_name _;',
+	require => Service['nginx'],
+}
