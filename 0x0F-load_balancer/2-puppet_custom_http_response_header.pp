db4f9f1131be3cb615b460187f912396af3b4bb1
baydre_africa <baydreafrica@gmail.com>
2023-12-30 07:47:13 +0100

    update: script that adds custom http header using puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,27 +1,34 @@
-# script that adds custom http header using puppet.
+# script that adds custom http header using puppet
 
+# Update system 
 exec {'update_system':
   command => '/usr/bin/apt-get -y update',
 }
-
+# Install Nginx package
 package { 'nginx':
   ensure => 'installed',
 }
+# Get the hostname of the server
+$hostname = $facts['hostname']
 
-service { 'nginx':
-  ensure  => 'running',
-  enable  => true,
+# Define Nginx configuration file path
+$nginx_config_file = '/etc/nginx/sites-available/default'
+
+# Configure Nginx with custom header using sed
+exec { 'configure_nginx':
+  command => "sed -i '51s|.*|add_header X-Served-By $hostname;|' $nginx_config_file",
+  path    => '/usr/bin',
   require => Package['nginx'],
+  notify  => Service['nginx'],
 }
 
-file { '/var/www/html/index.html' :
-  content => 'Hello World!',
+# Restart Nginx
+service { 'nginx':
+  ensure    => running,
+  enable    => true,
+  subscribe => Exec['configure_nginx'],
 }
 
-file_line { 'Creating header':
-  	ensure => present,
-  	path   => '/etc/nginx/sites-available/default',
-  	line   => "\tadd_header X-Served-By ${hostname};",
-  	after  => 'server_name _;',
-	require => Service['nginx'],
-}
+#notify { 'Configuration Complete':
+#  message => "Nginx configured with custom header X-Served-By on ${hostname}",
+#}
