10774b3a946e47a6fc19ca33608e61aa82ddc5a3
baydre_africa <baydreafrica@gmail.com>
2024-01-16 19:56:36 +0100

    function that queries an Reddit API and returns the total number of subscribers

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+function that queries the Reddit API
+& returns the number of subscribers
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    returns total number of subscribers
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-agent": "Mozilla/5.0"}
+
+    response = requests.get(url, headers=headers)
+
+    # if not a valid subreddit, return 0
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    return 0
